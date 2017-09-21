db98a1db1bde2a908cc0da7eba553ebbc7f0744b
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-09-21 01:57:54 -0500

    mostrarcolor

--- assets/js/estudiantes.js
+++ assets/js/estudiantes.js
@@ -3,10 +3,12 @@ console.log(arrayListaEstudiantes);
 
 $('#generarImput').click(generarImput);
 $('#mostrarLista').click(mostraListaEstudiantes);
+$('#porcentajeHse').click(updateDropOut);
 
 function generarImput(e) {
     $('#ingrasarDatosEstudiantes').append(`Nombre: <input id='nombre'></input>\
      PuntajeHSE: <input id='puntoHse'></input>\
+     PuntajeTEC: <input id='puntoTec'></input>\
      <button id='guardar' class='btn btn-primary'>Guardar</button> `);
     $('#guardar').click(agregarEstudiante)
 }
@@ -22,18 +24,19 @@ function agregarEstudiante() {
 }
 
 function mostraListaEstudiantes(l) {
-    arrayListaEstudiantes.map((elemento)=>{
-       
-        $('#divMostrarLista').append(`<p> Nombre: ${elemento.nombre}`,`<p>PuntosHse: ${ elemento.puntosHse}</p>`)
+    arrayListaEstudiantes.map((elemento) => {
+        $('#divMostrarLista').append(`<p> Nombre: ${elemento.nombre}`, `<p>PuntosHse: ${elemento.puntosHse}</p>`)
     });
     console.log('entro')
 }
 
-// function mostraListaEstudiantes() {
-//     arrayListaEstudiantes.map((elemento)=>{
-       
-//         $('#divMostrarLista').append(`<p>${elemento.nombre}---${ elemento.puntosHse}</p>`)
-//     });
-//     console.log('entro')
-// }
+function updateDropOut() {
+    nuevoArrayListaEstudiantes = arrayListaEstudiantes.filter(function (alumna) {
+        return (parseInt(alumna.puntosHse)) >= 70;
+        $('#updateDropout').append(`<p> Nombre: ${alumna.nombre}`, `<p>PuntosHse: ${alumna.puntosHse}</p>`)
+    });
+    console.log(nuevoArrayListaEstudiantes);
+}
+
+
 
